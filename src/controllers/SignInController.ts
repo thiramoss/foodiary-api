8be53822d421ed364b58c5798b77e067deae74bc
thiramoss <trs.tiba@gmail.com>
2025-12-16 22:05:50 -0300

    Validações com zod

--- src/controllers/SignInController.ts
+++ src/controllers/SignInController.ts
@@ -1,10 +1,23 @@
+import z from "zod";
 import type { HttpRequest, HttpResponse } from "../types/Http.js";
-import { ok } from "../utils/http.js";
+import { badRequest, ok } from "../utils/http.js";
+
+const schema = z.object({
+    email: z.email(),
+    password: z.string().min(8),
+})
 
 export class SignInController {
-    static async handle(request: HttpRequest): Promise<HttpResponse> {
+    static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+        const { success, error, data } = schema.safeParse(body);
+
+        if (!success) {
+            return badRequest({ errors: error.issues });
+        }
+
         return ok({
-            accessToken: 'Sign in: Token de aecsso',
+            data,
         })
     }
+
 }
--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,10 +1,31 @@
+import z from "zod";
 import type { HttpRequest, HttpResponse } from "../types/Http.js";
-import { created } from "../utils/http.js";
+import { badRequest, created } from "../utils/http.js";
+
+const schema = z.object({
+    goal: z.enum(['lose', 'maintain', 'gain']),
+    gender: z.enum(['male', 'female']),
+    birthDate: z.iso.date(),
+    height: z.number(),
+    weight: z.number(),
+    activityLevel: z.number().min(1).max(5),
+    account: z.object({
+        name:z.string().min(1),
+        email:z.email(),
+        password:z.string().min(8)
+    })
+})
 
 export class SignUpController {
-    static async handle(request: HttpRequest): Promise<HttpResponse> {
+    static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+        const {success, error, data} = schema.safeParse(body);
+
+        if(!success){
+            return badRequest({ errors: error.issues });
+        }
+
         return created({
-            accessToken: 'Sign up: Token de aecsso',
+            data,
         })
     }
 }
