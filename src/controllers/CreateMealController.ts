aab95e01b8baf04249eb444d74184ecceaaf8656
thiramoss <trs.tiba@gmail.com>
2026-01-04 00:43:56 -0300

    Upload pro S3

--- src/controllers/CreateMealController.ts
+++ src/controllers/CreateMealController.ts
@@ -1,8 +1,12 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { db } from "../db/index.js";
 import { mealsTable } from "../db/schema.js";
 import type { HttpResponse, ProtectedHttpRequest } from "../types/Http.js";
 import { badRequest, ok } from "../utils/http.js";
 import z from "zod";
+import { randomUUID } from "crypto";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { s3Client } from "../clients/s3Client.js";
 
 const schema = z.object({
     fileType: z.enum(['audio/m4a', 'image/jpeg'])
@@ -15,6 +19,18 @@ export class CreateMealController {
         if (!success) {
             return badRequest({ errors: error.issues });
         }
+
+        const fileId = randomUUID();
+        const ext = data.fileType === 'audio/m4a' ? '.m4a' : '.jpg';
+        const fileKey = `${fileId}${ext}`;
+
+        const command = new PutObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Key: fileKey,
+            ContentType: data.fileType,
+        });
+
+        const presignedURL = await getSignedUrl(s3Client, command, { expiresIn: 600});
 
         const [meal] = await db
             .insert(mealsTable)
@@ -32,6 +48,9 @@ export class CreateMealController {
             throw new Error("Failed to create meal record.");
         }
 
-        return ok({ mealId: meal.id });
+        return ok({ 
+            mealId: meal.id,
+            uploadURL: presignedURL,
+        });
     }
 }
