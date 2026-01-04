4e952c77e547f7b76a38e7740d9a909afd512d9d
thiramoss <trs.tiba@gmail.com>
2026-01-04 19:21:48 -0300

    S3 Trigger

---
+++ src/functions/fileUploadedEvent.ts
@@ -0,0 +1,9 @@
+import type { S3Event } from "aws-lambda";
+
+export async function handler(event: S3Event) {
+    await Promise.all(
+        event.Records.map(record => {
+            record.s3.object.key
+        }),
+    )
+}
