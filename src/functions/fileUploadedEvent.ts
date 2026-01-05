c03c0429a1275954e3d5f7fafb65d698be056525
thiramoss <trs.tiba@gmail.com>
2026-01-04 21:18:37 -0300

    Publicando as mensagens na fila

--- src/functions/fileUploadedEvent.ts
+++ src/functions/fileUploadedEvent.ts
@@ -1,9 +1,17 @@
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
 import type { S3Event } from "aws-lambda";
+import { sqsClient } from "../clients/sqsClient.js";
 
 export async function handler(event: S3Event) {
     await Promise.all(
-        event.Records.map(record => {
-            record.s3.object.key
+        event.Records.map(async record => {
+            const command = new SendMessageCommand({
+                QueueUrl: process.env.MEALS_QUEUE_URL,
+                MessageBody: JSON.stringify({ fileKey: record.s3.object.key})
+            });
+        
+            await sqsClient.send(command);
+            
         }),
     )
 }
